98a3bb985fc531d3d61072987f276ef29cad5a1c
Sirwit Yeesong <sirawit@nipa.cloud>
2018-01-30 17:19:28 +0700

    add install sh file

---
+++ install_jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#install
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+#config
+sudo systemctl start jenkins
+
+
